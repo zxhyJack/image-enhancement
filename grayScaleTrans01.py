9d478db9ae25dcbe652a42108da87f36a2cab879
zxhyJack <zhuhongjie0318@163.com>
2021-07-22 19:18:27 +0800

    修改E(Is)

--- grayScaleTrans01.py
+++ grayScaleTrans01.py
@@ -4,9 +4,7 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
-# 读入原始图像
-img = cv2.imread("./imageData/cameraman.tif", 1)
+from scipy.ndimage.filters import generic_filter, uniform_filter
 
 # 灰度化处理
 # img1=cv2.imread('girl.png',0)
@@ -16,6 +14,9 @@ img = cv2.imread("./imageData/cameraman.tif", 1)
 
 # 窗口大小
 kernel_size = 3
+popu_size = 10
+max_iter = 10
+dim = 4
 
 
 def generate_population(popu_size):
@@ -32,11 +33,22 @@ def generate_population(popu_size):
     return x
 
 
+# 窗口标准差
+def window_stdev(X, window_size):
+    (
+        r,
+        c,
+    ) = X.shape
+    X += np.random.rand(r, c) * 1e-6
+    c1 = uniform_filter(X, window_size, mode="reflect")
+    c2 = uniform_filter(X * X, window_size, mode="reflect")
+    return np.sqrt(c2 - c1 * c1)
+
+
 # 灰度变换
 def transform(img, a, b, c, k, n):
     newImg = copy.deepcopy(img)
-    rows = img.shape[0]
-    cols = img.shape[1]
+    rows, cols = img.shape
     D = 0
     for i in range(rows):
         for j in range(cols):
@@ -44,7 +56,6 @@ def transform(img, a, b, c, k, n):
     D = D / (rows * cols)
     for i in range(rows):
         for j in range(cols):
-            # gamma[i][j] = 3 * pow(gamma[i][j], 0.8)
             m = 0
             for x in range(i - 1, i + 2):
                 for y in range(j - 1, j + 2):
@@ -65,8 +76,7 @@ def transform(img, a, b, c, k, n):
 
 def customize_transform(img, a, b, c, k, kernel_size):
     # new_img = copy.deepcopy(img).astype(np.float)
-    rows = img.shape[0]
-    cols = img.shape[1]
+    rows, cols = img.shape
     C = 3
     pad = kernel_size // 2
     new_img = np.zeros((rows + pad * 2, cols + pad * 2, C), dtype=np.float)
@@ -75,7 +85,6 @@ def customize_transform(img, a, b, c, k, kernel_size):
     M = cv2.blur(img, (3, 3))
     D = np.sum(new_img) / 3
     D = D / (rows * cols)
-    print(D)
     for i in range(rows):
         for j in range(cols):
             sigma = np.sqrt(
@@ -91,6 +100,17 @@ def customize_transform(img, a, b, c, k, kernel_size):
     return new_img[pad : pad + rows, pad : pad + cols].astype(np.uint8)
 
 
+# 可以实现变换
+def ok_transform(img, a, b, c, k, k_size):
+    new_img = copy.deepcopy(img).astype(np.double)
+    D = np.mean(new_img)
+    M = cv2.blur(new_img, (3, 3))
+    S = window_stdev(new_img, k_size)
+    # S = generic_filter(new_img, np.std, size=k_size)
+    new_img = k * D / (S + b) * (new_img - c * M) + np.power(M, a)
+    return np.uint8(np.clip(new_img, 0, 255))  # 转换成0-255之间的数字
+
+
 # 计算图像的熵
 def calc_entropy(img):
     rows = img.shape[0]
@@ -104,7 +124,7 @@ def calc_entropy(img):
     # 统计不同灰度值像素的个数
     for i in range(rows):
         for j in range(cols):
-            val = img[i][j][0]
+            val = img[i][j]
             tmp[val] += 1
     # 计算每个灰度值像素出现的概率
     P = [0 for i in range(256)]
@@ -121,6 +141,7 @@ def calc_entropy(img):
 def calc_fitness(img):
     rows = img.shape[0]
     cols = img.shape[1]
+    E = np.sum(img)
     sobelX = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
     sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
     absx = cv2.convertScaleAbs(sobelX)
@@ -131,58 +152,58 @@ def calc_fitness(img):
     )  # 转化为二值化图像，图像阈值为100
     n_edgels = len(threshold[threshold == 255])  # 二值化图像大于阈值的像素个数
     entropy = calc_entropy(img)
-    return math.log(math.log(rows * cols)) * n_edgels / (rows * cols) * entropy
+    return math.log(math.log(E)) * n_edgels / (rows * cols) * entropy
 
 
-def PSOIE(popu_size, max_iter, dim, img, calc_fitness, w=0.6, c1=2, c2=2):
+def PSOIE(popu_size, max_iter, dim, img, calc_fitness, k_size, w=0.6, c1=2, c2=2):
     X = np.array(generate_population(popu_size))  # 初始化粒子群位置
     V = np.random.rand(popu_size, dim)  # 初始化粒子群速度
     pbest = X  # 初始化个体最优位置
+    print("===============初始化适应度===============")
     fitness = np.array(
-        [
-            calc_fitness(customize_transform(img, x[0], x[1], x[2], x[3], kernel_size))
-            for x in pbest
-        ]
+        [calc_fitness(ok_transform(img, x[0], x[1], x[2], x[3], k_size)) for x in pbest]
     )
+    print("===============适应度初始化完成===============")
+    print(fitness)
     gbest = X[np.argmax(fitness)]  # 初始化全局最优位置
+    print("gbest:", gbest)
+    print("================开始迭代===============")
+    figure_count = 0
     for iter in range(max_iter):
+        print("第========{}========次迭代".format(iter))
         for i in range(popu_size):
-            new_fitness = calc_fitness(
-                customize_transform(
-                    img, X[i][0], X[i][1], X[i][2], X[i][3], kernel_size
-                )
-            )
+            print("第{}个粒子".format(i))
+            out = ok_transform(img, X[i][0], X[i][1], X[i][2], X[i][3], k_size)
+            figure_count = figure_count + 1
+            # cv2.imshow('Figure.{}'.format(figure_count), out)
+            cv2.imwrite("result/cameraman/figure{}.png".format(figure_count), out)
+            new_fitness = calc_fitness(out)
             if new_fitness > fitness[i]:
                 pbest[i] = X[i]
                 fitness[i] = new_fitness
             if new_fitness > np.max(fitness):
                 gbest = X[i]
-            print(new_fitness, fitness[i])
+            print("pbest: ", pbest[i])
+        print("gbest: ", gbest)
         r1 = np.random.rand(popu_size, dim)
         r2 = np.random.rand(popu_size, dim)
         # 更新速度和权重
         V = w * V + c1 * r1 * (pbest - X) + c2 * r2 * (gbest - X)
         X = X + V
+        print("更新后粒子的位置：")
         print(X)
+        # print("更新后粒子的速度：")
+        # print(V)
     return gbest
 
 
-bestparam = PSOIE(10, 10, 4, img, calc_fitness)
-print(bestparam)
+# 读入原始图像
+img = cv2.imread("./imageData/cameraman.tif", 0)
+PSOIE(popu_size, max_iter, dim, img, calc_fitness, kernel_size)
 
 # 通过窗口展示图片 第一个参数为窗口名 第二个为读取的图片变量
-cv2.imshow("img", img)
-cv2.imshow(
-    "newImg",
-    customize_transform(
-        img, bestparam[0], bestparam[1], bestparam[2], bestparam[3], kernel_size
-    ),
-)
-# cv2.imshow("sobelX", sobelX)
-# cv2.imshow("sobelY", sobelY)
-# cv2.imshow("sobel", sobel)
-# cv2.imshow("threshold", threshold)
+# cv2.imshow("img", img)
 
 
-# 暂停cv2模块 不然图片窗口一瞬间即就会消失 观察不到
-cv2.waitKey(0)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
--- transform.py
+++ transform.py
@@ -2,7 +2,15 @@ import cv2
 import copy
 import time
 import numpy as np
-from scipy.ndimage.filters import generic_filter
+from scipy.ndimage.filters import generic_filter, uniform_filter
+
+
+def window_stdev(X, window_size):
+    r, c, l = X.shape
+    X += np.random.rand(r, c, l) * 1e-6
+    c1 = uniform_filter(X, window_size, mode="reflect")
+    c2 = uniform_filter(X * X, window_size, mode="reflect")
+    return np.sqrt(c2 - c1 * c1)
 
 
 def transform(img, a, b, c, k, k_size):
@@ -10,31 +18,25 @@ def transform(img, a, b, c, k, k_size):
     rows, cols, C = img.shape
     D = np.mean(new_img)
     M = cv2.blur(new_img, (3, 3))
-    S = generic_filter(new_img, np.std, size=3)
-    print(D)
-    print(M[:5, :5])
-    print(S[:5, :5])
-    pad = k_size // 2
+    # S = generic_filter(new_img, np.std, size=3)
+    S = window_stdev(new_img, 3)
+    # pad = k_size // 2
     # new_img = np.zeros((rows + pad * 2, cols + pad * 2, C), dtype=np.double)
     # new_img[pad : pad + rows, pad : pad + cols] = img.copy().astype(np.double)
     # tmp = new_img.copy()
-    # cv2.imshow('M', M)
-    # sigma = new_img.copy()
+    # S = new_img.copy()
     # M = new_img.copy()
-    # M = cv2.blur(img, (3, 3))
-    # D = np.sum(new_img) / 3
-    # D = D / (rows * cols)
     # for i in range(rows):
     #     for j in range(cols):
     #         M[pad + i][pad + j] = np.mean(tmp[i : i + k_size, j : j + k_size])
     # for i in range(rows):
     #     for j in range(cols):
     #         # M[pad + i][pad + j] = np.mean(tmp[i : i + k_size, j : j + k_size])
-    #         sigma[pad + i][pad + j] = np.sqrt(
-    #             np.mean(
-    #                 np.square(tmp[i : i + k_size, j : j + k_size] - M[pad + i][pad + j])
-    #             )
+    #         S[pad + i][pad + j] = np.sqrt(
+    #             np.mean(np.square(tmp[i : i + k_size, j : j + k_size] - M[i][j]))
     #         )
+    # S = S[pad : pad + rows, pad + cols]
+    # new_img = img.copy().astype(np.double)
     # for i in range(rows):
     #     for j in range(cols):
     #         # for l in range(C):
@@ -46,14 +48,13 @@ def transform(img, a, b, c, k, k_size):
     #             sigma + b
     #         ) + np.power(M[i][j], a)
     new_img = k * D / (S + b) * (new_img - c * M) + np.power(M, a)
-
     # print(M.astype(np.uint8)[0:5][0:5])
     # print(cv2.blur(img, (3, 3))[0:4][0:4])
     # cv2.imshow('M',M.astype(np.uint8))
     # cv2.imshow('sigma',sigma.astype(np.uint8))
     # cv2.imshow('new img',new_img.astype(np.uint8))
     # return new_img[pad : pad + rows, pad : pad + cols].astype(np.uint8)
-    return np.uint8(np.clip(new_img,0,255))
+    return np.uint8(np.clip(new_img, 0, 255))  # 转换成0-255之间的数字
 
     # for i in range(rows):
     #     for j in range(cols):
@@ -87,7 +88,6 @@ start = time.time()
 new_img = transform(img, 0.91, 0.3, 0.86, 1.3, 3)
 end = time.time()
 print(end - start)
-print("new img", new_img[:5, :5])
 
 cv2.imshow("original", img)
 cv2.imshow("new_img", new_img)
